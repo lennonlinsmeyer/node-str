69ca941dc3c77158c85af2d53a3ce84a75ede3bc
Lennon Linsmeyer <lennon7755@gmail.com>
2022-07-13 12:39:08 -0700

    mongoose and product model

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+mongoose.connect('mongodb://localhost:27017/ndstr');
+
 // Carrega as rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
-
+    
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
