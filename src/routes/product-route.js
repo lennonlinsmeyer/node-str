aa0fe67628dde0a40565e704f8840b4bcc370bc1
Lennon Linsmeyer <lennon7755@gmail.com>
2022-07-13 19:51:45 -0700

    get active products and get active product by slug

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,6 +4,8 @@ const express = require('express');
 const controller = require('../controllers/product-controller')
 const router = express.Router();
 
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
