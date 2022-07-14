733893c65f3ab877000ae7d664a1345bd4d252f4
Lennon Linsmeyer <lennon7755@gmail.com>
2022-07-13 19:59:57 -0700

    get product by id

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
+router.get('/id/:id', controller.getById);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
